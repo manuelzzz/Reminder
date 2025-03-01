9b253be1cccde06975093d54f02d98faf829bd08
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-03-01 15:33:53 -0300

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Manuel Santos Souza on 01/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
